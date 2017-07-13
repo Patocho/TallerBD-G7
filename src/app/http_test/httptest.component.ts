752eea9c4814e97a6f22221fe27609ccc5455a32
patocho <patovaldivia4@gmail.com>
2017-07-13 02:23:16 -0400

    actualizacion

--- src/app/http_test/httptest.component.ts
+++ src/app/http_test/httptest.component.ts
@@ -1,19 +1,53 @@
 import {Component} from '@angular/core';
 import {Httptestservice} from "./httptest.service"
 import {AreaChartConfig} from '../area-chart/area-chart-config';
+
 @Component({
   selector: 'httptest',
   template:`
+  <div style ="background-color: #d8ecf7; border: 1px solid #afcde3">
   			<label for="range">Range:</label>
+<style type="text/css">
+  button {
+ border: none;
+ background: #3a7999;
+ color: #f2f2f2;
+ padding: 10px;
+ font-size: 18px;
+ border-radius: 5px;
+ position: relative;
+ box-sizing: border-box;
+ transition: all 500ms ease;
+}
+button:before {
+ content:'';
+ position: absolute;
+ top: 0px;
+ left: 0px;
+ width: 0px;
+ height: 42px;
+ background: rgba(255,255,255,0.3);
+ border-radius: 5px;
+ transition: all 2s ease;
+}
+
+button:hover:before {
+ width: 100%;
+}
+  </style>
+
 	<input name="range" type="text" [(ngModel)]="range" placeholder="range" />
 	<button (click)="onTestGet()">GET Tweets</button>
 	<button (click)="onGetStats()">GET Stats</button>
 	<button (click)="onGenerateDataViz()">GET Stats + DataViz</button>
 	<button (click)="delete()">Delete JSON</button>
+		<p>Output: {{getData}}</p>
 	<br>
-	<p>Output: {{getData}}</p>
 	<br>
-	<app-area-chart [config]="areaChartConfig"></app-area-chart>	
+	<app-area-chart [config]="areaChartConfig"></app-area-chart>
+	</div>
+
+		
     ` ,
   providers: [Httptestservice]		
 })
--- src/app/http_test/httptest.service.ts
+++ src/app/http_test/httptest.service.ts
@@ -19,7 +19,7 @@ export class Httptestservice{
 		headers.append('Content-Type', 
 		'application/x-www-form-urlencoded');
 
-		return this._http.post('http://146.83.216.216:3004/explorer/#!/Tweet/Tweet_create', params, {
+		return this._http.post('http://0.0.0.0:3000/explorer/#!/Tweet/Tweet_create', params, {
 			headers: headers
 		})
 		.map(res => res.json());
@@ -27,7 +27,7 @@ export class Httptestservice{
 	}
 
 	getStats(range: string){
-		return this._http.get('http://146.83.216.216:3004/api/Tweets/stats?range='+range).map((response:Response) => response.json());
+		return this._http.get('http://0.0.0.0:3000/api/Tweets/stats?range='+range).map((response:Response) => response.json());
 	}
 
 
